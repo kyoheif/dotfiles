cc13418a4f48cb2a14efe20a660bca2158e25b83
Kyohei FUSHIDA <kyohei.fushida@gmail.com>
2021-01-01 02:59:35 +0900

    create install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+GNORE_PATTERN="^\.(git|travis)"
+
+echo "Create dotfile links."
+for dotfile in .??*; do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+done
+echo "Success"
+
